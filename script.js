7a62315ba889ba2ad933079f075b76ffa2760506
Tringalix <davide.tringali2006@gmail.com>
2026-02-24 11:24:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Per ora lasciamo un file JS minimale.
+// Qui potrai in futuro aggiungere:
+// - logica dei test
+// - interazioni con i tool
+// - animazioni leggere
+
+console.log("UniFlow ready. Trova il tuo flow. 🚀");
